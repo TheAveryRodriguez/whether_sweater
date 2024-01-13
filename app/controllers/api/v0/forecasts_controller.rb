7889aff4d83165886d34041a7e1364b49b5cb6d3
Edward Rodriguez <rodriguez.avery@gmail.com>
2024-01-13 13:27:08 -0600

    update to specs, credentials

--- app/controllers/api/v0/forecasts_controller.rb
+++ app/controllers/api/v0/forecasts_controller.rb
@@ -1,4 +1,11 @@
 class Api::V0::ForecastsController < ApplicationController
   def show
+    conn = Faraday.new(url: "") do |faraday|
+      faraday.headers[] = Rails.application.credentials.mapquest[:key]
+    end
+
+    response = conn.get("")
+
+    data = JSON.parse(response.body, symbolize_names: true)
   end
 end
---
+++ spec/requests/api/v0/forecast_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper"
+
+describe Api::V0::ForecastsController, type: :controller do
+  describe "#show" do
+  end
+end

643deb200a578ec9b3683c6c9326a3c9db30ac76
Edward Rodriguez <rodriguez.avery@gmail.com>
2024-01-14 13:04:31 -0600

    middle of testing

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,9 +1,31 @@
 require "rails_helper"
 
-RSpec.describe ForecastFacade do
-  let(:forecast_info) { ForecastFacade.forecast_info(location) }
+RSpec.describe ForecastFacade, type: :facade do
+  describe ".forecast_info" do
+    let(:location) { "cincinnati,oh" }
+    let(:coordinates) { {lat: 39.1031, lon: 84.5120} }
+    let(:forecast) { {temperature: 75, conditions: "Sunny"} }
 
-  it "creates forecast poros", :vcr do
-    expect(Forecast.first).to be_a(Forecast)
+    before do
+      allow(MapquestService).to receive(:get_coordinates).with(location).and_return(coordinates)
+      allow(WeatherService).to receive(:get_forecast).with(coordinates).and_return(forecast)
+    end
+
+    it "calls MapquestService.get_coordinates with the correct parameters" do
+      ForecastFacade.forecast_info(location)
+      expect(MapquestService).to have_received(:get_coordinates).with(location)
+    end
+
+    it "calls WeatherService.get_forecast with the correct parameters" do
+      ForecastFacade.forecast_info(location)
+      expect(WeatherService).to have_received(:get_forecast).with(coordinates)
+    end
+
+    it "returns a Forecast object" do
+      result = ForecastFacade.forecast_info(location)
+      expect(result).to be_a(Forecast)
+      expect(result.location).to eq(location)
+      expect(result.data).to eq(forecast)
+    end
   end
 end
--- spec/requests/api/v0/forecast_spec.rb
+++ spec/requests/api/v0/forecast_spec.rb
@@ -1,40 +1,34 @@
 require "rails_helper"
 
-RSpec.describe "???" do
+RSpec.describe "Forecast" do
   context "happy path endpoints" do
     it "can get merchants for case insensitive name search" do
-      create(:merchant, name: "Largemouth Bass")
-      create(:merchant, name: "Smallmouth Bass")
-      create(:merchant, name: "White Bass")
-      create(:merchant, name: "Striped Bass")
-      create(:merchant, name: "Spotted Bass")
-      create(:merchant, name: "Sea Bass")
       create(:merchant, name: "Channel Catfish")
 
-      get "/api/v1/merchants/find_all?name=bAsS"
+      get "/api/v0/forecast"
 
-      merchant_json = JSON.parse(response.body, symbolize_names: true)
+      # merchant_json = JSON.parse(response.body, symbolize_names: true)
 
-      merchant = merchant_json[:data]
+      # merchant = merchant_json[:data]
 
-      expect(response).to be_successful
+      # expect(response).to be_successful
 
-      expect(merchant).to be_a(Array)
-      expect(merchant.first[:attributes]).to be_a(Hash)
-      expect(merchant.first[:attributes]).to have_key(:name)
-      expect(merchant.first[:attributes][:name]).to eq("Largemouth Bass")
+      # expect(merchant).to be_a(Array)
+      # expect(merchant.first[:attributes]).to be_a(Hash)
+      # expect(merchant.first[:attributes]).to have_key(:name)
+      # expect(merchant.first[:attributes][:name]).to eq("Largemouth Bass")
     end
   end
 
   context "sad path endpoints" do
     it "gets empty array for merchants case insensitive name search" do
-      get "/api/v1/merchants/find_all?name=name"
+      get "/api/v0/forecast"
 
-      merchant_json = JSON.parse(response.body, symbolize_names: true)
+      # merchant_json = JSON.parse(response.body, symbolize_names: true)
 
-      expect(response).to be_successful
-      expect(merchant_json[:data]).to be_a(Array)
-      expect(merchant_json[:data].empty?).to be(true)
+      # expect(response).to be_successful
+      # expect(merchant_json[:data]).to be_a(Array)
+      # expect(merchant_json[:data].empty?).to be(true)
     end
   end
 end

6948a20fc76356b86572346df7e5776340609e55
Edward Rodriguez <rodriguez.avery@gmail.com>
2024-01-15 14:18:26 -0600

    create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,7 +7,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v0 do
-      get "/forecast", to: "forecasts#index"
+      # get "/forecast", to: "forecasts#index"
+      resources :forecasts, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end

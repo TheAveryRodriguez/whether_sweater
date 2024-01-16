86ab6fdcff29198f4651080d67b6cb4c0274cc75
Edward Rodriguez <rodriguez.avery@gmail.com>
2024-01-15 21:01:32 -0600

    updated testing and deleted irrelavent file

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v0 do
-      # get "/forecast", to: "forecasts#index"
-      resources :forecasts, only: [:index]
+      get "/forecast", to: "forecasts#index"
       resources :users, only: [:create]
     end
   end
